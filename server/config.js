27aa7f30c611c6b80099c6e9b600c2312f27df97
DHRUVAL GUPTA <72140427+dhruvalgupta2003@users.noreply.github.com>
2023-07-06 23:09:17 +0530

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,6 +1,1 @@
-export default {
-    JWT_SECRET : "3ff8bc7c2db194d46310e6f9f80a057971d8a82ab8fced317dba315141383d2d",
-    EMAIL: "erenjeager5602@gmail.com", // testing email & password
-    PASSWORD : "zxtuohjbqffhcytg",
-    ATLAS_URI: "mongodb+srv://dhruvalgupta2003:dkgupta1234@cluster1.jw7aqks.mongodb.net/?retryWrites=true&w=majority"
-};
+
